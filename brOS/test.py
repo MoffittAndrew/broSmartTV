ae46a5f3ab7d26efb81baf72df34be653d74e124
MoffittAndrew <moffittandrew@outlook.com>
2024-07-26 14:01:03 +0100

    test chromium script

--- brOS/test.py
+++ brOS/test.py
@@ -1,2 +1,17 @@
-print("Testing")
-print("Testing 2")
+#!/usr/bin/env python
+
+from selenium import webdriver
+from pyvirtualdisplay import Display
+
+print ('Starting ...')
+display = Display(visible=0, size=(1600, 1200))
+display.start()
+driver = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver')
+print ('webdriver loaded')
+
+# Navigate to target website
+driver.get('https://www.youtube.com')
+
+driver.save_screenshot('SeleniumChromiumTest.png')
+print ('target page loaded adnd screenshot taken')
+print ('Done')
