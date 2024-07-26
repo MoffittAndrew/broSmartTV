f280a96c3f718a2b9204a4cea5ec8f94afd5050c
MoffittAndrew <moffittandrew@outlook.com>
2024-07-26 16:08:58 +0100

    testing

--- brOS/screen_test.py
+++ brOS/screen_test.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow
 
 # Only needed for access to command line arguments
 import sys
@@ -8,8 +8,15 @@ import sys
 # If you know you won't use command line arguments QApplication([]) works too.
 app = QApplication(sys.argv)
 
+screen = app.primaryScreen()
+print('Screen: %s' % screen.name())
+size = screen.size()
+print('Size: %d x %d' % (size.width(), size.height()))
+rect = screen.availableGeometry()
+print('Available: %d x %d' % (rect.width(), rect.height()))
+
 # Create a Qt widget, which will be our window.
-window = QWidget()
+window = QMainWindow()
 window.show()  # IMPORTANT!!!!! Windows are hidden by default.
 
 # Start the event loop.
