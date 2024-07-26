fb4a6703fb440297f442cfe0f10e3c100ec88398
MoffittAndrew <moffittandrew@outlook.com>
2024-07-26 16:14:16 +0100

    adding a button

--- brOS/screen_test.py
+++ brOS/screen_test.py
@@ -1,5 +1,5 @@
 from PyQt5.QtCore import QSize
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
 
 # Only needed for access to command line arguments
 import sys
@@ -15,6 +15,8 @@ rect = screen.availableGeometry()
 # Create a Qt widget, which will be our window.
 window = QMainWindow()
 window.setFixedSize(QSize(rect.width(), rect.height()))
+button = QPushButton("Press Me!")
+window.setCentralWidget(button)
 window.show()  # IMPORTANT!!!!! Windows are hidden by default.
 
 # Start the event loop.
