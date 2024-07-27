5b9326d83255b16d9fe55e9d5cca633de2746c56
MoffittAndrew <moffittandrew@outlook.com>
2024-07-27 22:55:10 +0100

    adding delay

--- brOS/remote_test.py
+++ brOS/remote_test.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as GPIO
+from time import sleep
 
 GPIO.setmode(GPIO.BCM)
 INPUT_PIN = 14
@@ -11,6 +12,7 @@ while True:
     if not GPIO.input(INPUT_PIN) and not IR_on:
         print("IR detected!")
         IR_on = True
+        sleep(1)
     elif IR_on:
         print()
         IR_on = False
