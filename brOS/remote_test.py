3595ece76a4a60d80c9f5f38e73967ca8c76db87
MoffittAndrew <moffittandrew@outlook.com>
2024-08-01 22:05:34 +0100

    added more IR conf files

--- brOS/remote_test.py
+++ brOS/remote_test.py
@@ -1,18 +1,3 @@
-import RPi.GPIO as GPIO
-from time import sleep
+from os import system as cmd
 
-GPIO.setmode(GPIO.BCM)
-INPUT_PIN = 14
-
-# Infrared reciever
-GPIO.setup(INPUT_PIN, GPIO.IN)
-
-IR_on = False
-while True:
-    if not GPIO.input(INPUT_PIN) and not IR_on:
-        print("IR detected!")
-        IR_on = True
-        sleep(1)
-    elif IR_on:
-        print()
-        IR_on = False
+cmd("")
