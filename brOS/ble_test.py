1751c6636f3a61738a5085801df15aa3ce27ab3d
MoffittAndrew <moffittandrew@outlook.com>
2024-08-09 22:15:56 +0100

    testing characteristic

--- brOS/ble_test.py
+++ brOS/ble_test.py
@@ -1,25 +1,28 @@
 import struct
-import uuid
 from bluepy.btle import Peripheral
 
 remote_mac = "28:cd:c1:0d:f6:62"
+
+def _decode_button(data):
+    return struct.unpack("<h", data)[0] / 100
 
 print(f"Searching for device with MAC address '{remote_mac}'...")
 
 remote = Peripheral(remote_mac)
 #service = remote.getServiceByUUID("0x1849")
-#charcteristic = remote.getCharacteristics(uuid=uuid.UUID("0x7660ca10"))
+charcteristic = remote.getCharacteristics()[-1]
 #descriptors = remote.getDescriptors()
 
 #print(service)
 #print(charcteristic)
 #print(descriptors)
 
-def _decode_button(data):
-    return struct.unpack("<h", data)[0] / 100
+print("Connected")
 
 while True:
     remote.waitForNotifications(1)
-    read = remote.readCharacteristic("0x7660ca10")
+    read = charcteristic.read()
+    print()
+    print(charcteristic.uuid)
     print(read)
     print(_decode_button(read))
