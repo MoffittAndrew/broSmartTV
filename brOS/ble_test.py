fe79da3588c99c67ccadb6a757c553faf99408fb
MoffittAndrew <moffittandrew@outlook.com>
2024-08-09 21:40:43 +0100

    adding decode_button

--- brOS/ble_test.py
+++ brOS/ble_test.py
@@ -1,6 +1,9 @@
+import struct
 from bluepy.btle import Peripheral
 
 remote_mac = "28:cd:c1:0d:f6:61"
+
+print(f"Searching for device with MAC address '{remote_mac}'")
 
 remote = Peripheral(remote_mac)
 service = remote.getServiceByUUID("0x1849")
@@ -11,6 +14,11 @@ print(service)
 print(charcteristics)
 print(descriptors)
 
+def _decode_button(data):
+    return struct.unpack("<h", data)[0] / 100
+
 while True:
     remote.waitForNotifications()
-    print(remote.readCharacteristic(charcteristics[0]))
+    read = remote.readCharacteristic(charcteristics[0])
+    print(read)
+    print(_decode_button(read))
