d062539b4faeaa8cb1f5b903b7252bd9a48b2dcd
MoffittAndrew <moffittandrew@outlook.com>
2024-08-09 22:22:29 +0100

    updated interval to 0.1s

--- brOS/ble_test.py
+++ brOS/ble_test.py
@@ -4,25 +4,25 @@ from bluepy.btle import Peripheral
 remote_mac = "28:cd:c1:0d:f6:62"
 
 def _decode_button(data):
-    return struct.unpack("<h", data)[0] / 100
+    return struct.unpack("<h", data)[0]
 
 print(f"Searching for device with MAC address '{remote_mac}'...")
 
 remote = Peripheral(remote_mac)
 #service = remote.getServiceByUUID("0x1849")
-charcteristic = remote.getCharacteristics()[-1]
+charcteristic = remote.getCharacteristics(uuid="00002ba5-0000-1000-8000-00805f9b34fb")
 #descriptors = remote.getDescriptors()
 
 #print(service)
 #print(charcteristic)
 #print(descriptors)
 
-print("Connected")
+print("Connected\n")
 
+read_backup = 0
 while True:
-    remote.waitForNotifications(1)
+    remote.waitForNotifications(0.1)
     read = charcteristic.read()
-    print()
-    print(charcteristic.uuid)
-    print(read)
-    print(_decode_button(read))
+    if read != read_backup:
+        print(_decode_button(read))
+        read_backup = read
