ab7f37c7c511f7db7223e7f58a9706aeab12cce8
MoffittAndrew <moffittandrew@outlook.com>
2024-08-10 19:25:01 +0100

    updated ble_test2

--- brOS/ble_test2.py
+++ brOS/ble_test2.py
@@ -7,42 +7,43 @@ _SERVICE_UUID = "0000{0:x}-0000-1000-8000-00805f9b34fb".format(0x1849)
 _CHARACTERISTIC_UUID = "0000{0:x}-0000-1000-8000-00805f9b34fb".format(0x2BA5)
 
 
-def _decode_temperature(data):
-    return struct.unpack("<h", data)[0] / 100
+def _decode_data(data):
+    return struct.unpack("<h", data)[0]
 
 
 def _callback(sender: bleak.BleakGATTCharacteristic, data: bytearray):
-    data = None if not data else _decode_temperature(data)
+    data = None if not data else _decode_data(data)
     print(f"{sender}: {data}")
 
 
 async def find_temp_sensor():
-    name = 'mpy-temp'
+    name = 'bro-ito'
     return await bleak.BleakScanner.find_device_by_name(name)
 
 
 async def do_connect():
-    print("Start scanning for temperature sensor device")
+    print("Scanning for remote...")
     device = await find_temp_sensor()
     if not device:
-        print("Temperature sensor not found")
+        print("Remote not found")
         return
 
     async with bleak.BleakClient(device) as client:
-        # Get the service:
         service = client.services.get_service(_SERVICE_UUID)
         if service is None:
-            print("Temperature service not found")
+            print("Service not found")
             return
 
         characteristic = service.get_characteristic(_CHARACTERISTIC_UUID)
         if characteristic is None:
-            print("Temperature characteristic not found")
+            print("Characteristic not found")
             return
 
         await client.start_notify(characteristic, _callback)
         while client.is_connected:
             await asyncio.sleep(5)
+    
+    print("Disconnected.")
 
 
 async def main():
