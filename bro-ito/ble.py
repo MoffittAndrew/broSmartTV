346d2a2789336e60f4c8b1137e9dd9508bc49225
MoffittAndrew <moffittandrew@outlook.com>
2024-08-16 17:41:44 +0100

    swapped menu and return

--- bro-ito/ble.py
+++ bro-ito/ble.py
@@ -1,6 +1,8 @@
+# this code is absolute dogshit lmao, but it works
+
 # Only import what is necessary
 # saves memory and power
-from asyncio import sleep_ms, create_task, run
+from asyncio import sleep_ms, create_task, run, CancelledError
 from aioble import Service as aioble_Service, Characteristic as aioble_Characteristic, register_services as aioble_register_services, advertise as aioble_advertise
 from bluetooth import UUID, BLE
 from micropython import const
@@ -23,6 +25,8 @@ _INIT_WAIT_MS = 2250
 _SEND_INTERVAL_MS = 50
 # How frequently to check for button presses
 _BUTTON_INTERVAL_MS = 25
+# Stop advertising after this many seconds of no connection
+_ADV_TIMEOUT = 10
 # Disconnect after this many seconds of inactivity
 _INACTIVITY_TIMEOUT = 300
 
@@ -33,8 +37,8 @@ button_lookup = [
     "MIC",
     "VOL_DOWN",
     "VOL_UP",
-    "RETURN",
     "MENU",
+    "RETURN",
     "NAV_LEFT",
     "SELECT",
     "NAV_UP",
@@ -72,32 +76,44 @@ class LED:
         
         
         
-class Inactivity_Timer:
-    def __init__(this):
-        print()
+class Timeout:
+    def __init__(this, start_val, timeout_msg):
+        this.__start_val = start_val
+        this.__timeout_msg = timeout_msg
+        this.__check = _true_check
+        this.__counting = False
+        
+    def set_check(this, check):
+        this.__check = check
         
     def reset(this):
-        this.__timer = _INACTIVITY_TIMEOUT
+        this.__timer = this.__start_val
+        
+    def stop(this):
+        this.__counting = False
 
-    async def start_countdown(this, conn):
+    async def start(this):
         this.reset()
-        while this.__timer > 0 and conn.is_connected():                
+        this.__counting = True
+        while this.__counting and this.__timer > 0 and this.__check():                
             
-            await _sleep_ms(1000)
+            await sleep_ms(1000)
             this.__timer -= 1
             
-        if conn.is_connected():
-            print(f"No activity detected in past {_INACTIVITY_TIMEOUT} seconds, disconnecting BLE...")
+        this.__counting = False
+        if this.__check():
+            print(this.__timeout_msg)
 
 
 
-class Signal_Sender:
+class SignalSender:
     def __init__(this, char, inactivity_timer):
         this.__char = char
         this.__inactivity_timer = inactivity_timer
         this.__conn = None
         this.__backlog = []
         this.__processing = False
+        this.__process_backlog_task = None
         
     def __send(this, data):
         this.__char.notify(this.__conn, data)
@@ -115,7 +131,8 @@ class Signal_Sender:
         
     def stop(this):
         this.__processing = False
-        this.__process_backlog_task.cancel()
+        if this.__process_backlog_task:
+            this.__process_backlog_task.cancel()
         
     async def __process_backlog(this):
         await sleep_ms(_INIT_WAIT_MS)
@@ -130,6 +147,42 @@ class Signal_Sender:
                     del this.__backlog[0]
                 else:
                     print("Cannot process signal backlog, no connection established!")
+                    
+
+class Connection:
+    def __init__(this):
+        this.__conn = None
+        this.__adv = True
+        this.__adv_timeout_timer = Timeout(_ADV_TIMEOUT, "Advertising timed out.")
+        this.__adv_timeout_timer.set_check(this.get_adv)
+        
+    def get_adv(this):
+        return this.__adv
+        
+    async def __get_conn(this):
+        try:
+            this.__conn = await aioble_advertise(
+                _ADV_INTERVAL_MS,
+                name=_REMOTE_NAME,
+                services=[_SERVICE_UUID],
+                appearance=_ADV_APPEARANCE_GENERIC_REMOTE_CONTROL,
+            )
+            this.__adv_timeout_timer.stop()
+            this.__adv = False
+        except CancelledError:
+            this.__conn = None
+    
+    async def connect(this):
+    
+        print("Advertising...")
+        conn_task = create_task(this.__get_conn())
+        await this.__adv_timeout_timer.start()
+        
+        return this.__conn
+
+
+def _true_check():
+    return True
 
 
 def _encode_data(data):
@@ -158,6 +211,7 @@ async def read_button_task(ble_sender, buttons, active_button_index):
                     # Send signal when button is released
                     ble_sender.send_signal(f"RELEASED_{button_lookup[i]}")
                 button_backup[i] = val
+    
 
 async def run_ble(buttons, active_button_index):
     
@@ -171,32 +225,31 @@ async def run_ble(buttons, active_button_index):
     )
     aioble_register_services(remote_service)
     
-    inactivity_timer = Inactivity_Timer()
     
-    ble_sender = Signal_Sender(remote_characteristic, inactivity_timer)
+    inactivity_timer = Timeout(_INACTIVITY_TIMEOUT, f"No activity detected in past {_INACTIVITY_TIMEOUT} seconds, disconnecting BLE...")
+    
+    ble_sender = SignalSender(remote_characteristic, inactivity_timer)
     button_task = create_task(read_button_task(ble_sender, buttons, active_button_index))
     
-    print("Advertising...")
-    async with await aioble_advertise(
-        _ADV_INTERVAL_MS,
-        name=_REMOTE_NAME,
-        services=[_SERVICE_UUID],
-        appearance=_ADV_APPEARANCE_GENERIC_REMOTE_CONTROL,
-    ) as connection:
+    global_connection = Connection()
+    connection = await global_connection.connect()
+    if connection:
+        
         print(f"Connection from {connection.device}")
         ble_sender.start_processing(connection)
         await sleep_ms(_INIT_WAIT_MS)
-        led.on()
-        await inactivity_timer.start_countdown(connection)
         
-    print("Disconnected.")
+        led.on()
+        await inactivity_timer.start()
+        
     button_task.cancel()
     ble_sender.stop()
     led.off()
-    
-    print("Shutting down BLE...")
-    BLE().active(False)
-    
+
+
 def main(buttons, active_button_index):
     run(run_ble(buttons, active_button_index))
+    print("Disconnected.")
+    print("Shutting down BLE...")
+    BLE().active(False)
     print("Entering sleep mode...")
