790259661ba118541c00cac27cff2c1344b77798
MoffittAndrew <moffittandrew@outlook.com>
2025-07-06 22:35:55 +0100

    Not sure why this isnt working

--- py/input_interface.py
+++ py/input_interface.py
@@ -138,8 +138,7 @@ class InputInterface(QLabel):
             this.getWebDriver().quit()
     
     def paintEvent(this, event=None):
-        painter = QtGui.QPainter()
-        painter.begin(this)
+        painter = QtGui.QPainter(this)
         painter.setRenderHint(QtGui.QPainter.Antialiasing, True)
 
         painter.setPen(QtGui.QPen(Qt.red, 5, Qt.SolidLine))
