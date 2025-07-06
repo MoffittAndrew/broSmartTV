bbbdf15d84c8c995b14e9777a362d2b1e8860606
MoffittAndrew <moffittandrew@outlook.com>
2025-07-06 22:02:48 +0100

    testing adding super()

--- py/input_interface.py
+++ py/input_interface.py
@@ -140,6 +140,7 @@ class InputInterface(QLabel):
             this.getWebDriver().quit()
     
     def paintEvent(this, event=None):
+        super.paintEvent(event)
         painter = QtGui.QPainter()
         painter.begin(this)
         painter.setRenderHint(QtGui.QPainter.Antialiasing, True)
