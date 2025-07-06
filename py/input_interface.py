4a47969734dae46f372ac297bb9eb7740929987c
MoffittAndrew <moffittandrew@outlook.com>
2025-07-06 22:33:03 +0100

    fixing QPainter

--- py/input_interface.py
+++ py/input_interface.py
@@ -138,7 +138,7 @@ class InputInterface(QLabel):
             this.getWebDriver().quit()
     
     def paintEvent(this, event=None):
-        painter = QtGui.QPainter(this)
+        painter = QtGui.QPainter()
         painter.begin(this)
         painter.setRenderHint(QtGui.QPainter.Antialiasing, True)
 
