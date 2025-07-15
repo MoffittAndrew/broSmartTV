e33fb3848fca63aceff4c377f4c04f789ec54c8e
MoffittAndrew <moffittandrew@outlook.com>
2025-07-15 12:32:56 +0100

    Testing more remote stuff

--- py/interface/remote_interface.py
+++ py/interface/remote_interface.py
@@ -140,7 +140,7 @@ class RemoteInterface:
         asyncio.create_task(this.init())
         while this.isRunning() and not this.isConnected():
             await asyncio.sleep(this.getCheckAliveInterval())
-            
+            print("Waiting...")
         print("Done")
 
 remoteInterface = RemoteInterface()
