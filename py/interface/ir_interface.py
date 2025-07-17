d70abb36832ae18f44a335c52905572a42d9ca7d
MoffittAndrew <moffittandrew@outlook.com>
2025-07-17 22:58:45 +0100

    adding discrete power off

--- py/interface/ir_interface.py
+++ py/interface/ir_interface.py
@@ -1,10 +1,12 @@
 print("Importing infrared interface...")
 
+import os
+
 class IRInterface:
     def __init__(this, *args, **kwargs):
         ...
     
     def send(this, data):
-        ...
+        os.system(f"sudo irsend SEND_ONCE Epson_12807990 {data}")
 
 irInterface = IRInterface()
