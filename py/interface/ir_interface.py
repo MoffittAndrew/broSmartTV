ad6db806118772a87cc10486898ce4e710b3ac77
MoffittAndrew <moffittandrew@outlook.com>
2026-07-05 18:05:33 +0100

    Moving raspberry pi check to globals.py

--- py/interface/ir_interface.py
+++ py/interface/ir_interface.py
@@ -1,32 +1,12 @@
 print("Importing infrared interface...")
 
 import os
-import platform
 
-
-def _is_raspberry_pi_debian():
-    if platform.system() != "Linux":
-        return False
-
-    try:
-        with open("/etc/os-release", "r", encoding="utf-8") as f:
-            os_release = f.read().lower()
-    except OSError:
-        return False
-
-    is_debian = "id=debian" in os_release or "id_like=debian" in os_release
-    if not is_debian:
-        return False
-
-    try:
-        with open("/proc/device-tree/model", "r", encoding="utf-8", errors="ignore") as f:
-            return "raspberry pi" in f.read().lower()
-    except OSError:
-        return False
+from globals import DEVICE
 
 class IRInterface:
     def __init__(self, *args, **kwargs):
-        self._can_send_ir = _is_raspberry_pi_debian()
+        self._can_send_ir = DEVICE.IS_RASPBERRY_PI
     
     def send(self, data):
         if self._can_send_ir:
