589f15a58a0cfb529bdcae92f01713847ad0dcc2
MoffittAndrew <moffittandrew@outlook.com>
2026-09-03 15:00:30 +0100

    setting default projector volume to 5

--- py/interface/projector_interface.py
+++ py/interface/projector_interface.py
@@ -26,6 +26,10 @@ class ProjectorInterface:
     async def on(self):
         await self.send(PROJECTOR.CODES.ON)
         await self.send(PROJECTOR.CODES.RETURN)
+        for _ in range(10):
+            await self.volDown()
+        for _ in range(PROJECTOR.AUTO_VOL_SET):
+            await self.volUp()
     
     async def off(self):
         # for now
