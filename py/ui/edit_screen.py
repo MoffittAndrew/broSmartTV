b428d950d2742ca3cba5749d1ca6b33838a985a6
MoffittAndrew <moffittandrew@outlook.com>
2026-09-21 22:19:23 +0100

    adding helpful text for the not-yet-implemented features :)

--- py/ui/edit_screen.py
+++ py/ui/edit_screen.py
@@ -2,11 +2,16 @@ print("Importing edit screen...")
 
 from ui.gui import CustomQWidget
 
+from PyQt5.QtWidgets import QLabel
+
 class EditScreen(CustomQWidget):
     def __init__(self, navBarButton = None, *args, **kwargs):
         super().__init__(*args, **kwargs)
         
         self.setNavBarButton(navBarButton)
+        
+        self.__heading = QLabel("this feature isn't working yet dumbass")
+        self.__heading.setStyleSheet("font-size: 44px; font-weight: bold; color: white;")
         
     ## Getters
     
